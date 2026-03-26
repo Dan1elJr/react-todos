c77677ac3a79c8b6d2fced82c912e7fc5bc2780d
Daniel Junior <danieljunior1903@gmail.com>
2026-03-26 11:13:30 -0400

    React/Fundamentals - onclick Listener

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ const todos = [
   {rowNumber:5, rowDescription:'Take a sleep', rowAssigned:'User Five'}
 ];
 
+const addTodo = () => {
+  if(todos.length>0){
+    const newTodo = {
+      rowNumber: todos.length +1,
+      rowDescription: 'New Todo',
+      rowAssigned: 'User Three'
+    }
+    todos.push(newTodo)
+  }
+  
+  console.log(todos)
+}
+
 function App() {
   return (
     <div className='mt-5 container'>
@@ -20,6 +33,7 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={addTodo}>Add Todo</button>
         </div>
       </div>
     </div>
