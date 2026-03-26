f99159465fa206837b80ab858ac71b19bffc0f6f
Daniel Junior <danieljunior1903@gmail.com>
2026-03-26 11:46:01 -0400

    React/Fundamentals - Handling React data states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
 
-//Object array for input into component
-const todos = [
+function App() {
+  //Object array for input into component
+ const [todos,setTodos] = useState([
   {rowNumber:1, rowDescription:'Feed puppy', rowAssigned:'User One'},
   {rowNumber:2, rowDescription:'Water Plants', rowAssigned:'User Two'},
   {rowNumber:3, rowDescription:'Make dinner', rowAssigned:'User Three'},
   {rowNumber:4, rowDescription:'Make some money', rowAssigned:'User Four'},
   {rowNumber:5, rowDescription:'Take a sleep', rowAssigned:'User Five'}
-];
+]);
 
 const addTodo = () => {
   if(todos.length>0){
@@ -17,14 +18,13 @@ const addTodo = () => {
       rowNumber: todos.length +1,
       rowDescription: 'New Todo',
       rowAssigned: 'User Three'
-    }
-    todos.push(newTodo)
+    };
+    setTodos(todos=>[...todos,newTodo])
+    console.log(todos)
   }
   
-  console.log(todos)
+  
 }
-
-function App() {
   return (
     <div className='mt-5 container'>
       <div className='card'>
